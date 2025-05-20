25b13d769644a65e95916e4326ce93fa76f14410
Carlos Martins <carlosouza.ces@gmail.com>
2025-05-20 20:39:58 -0300

    Update and rename Questao_03.js to questao_03.js

---
+++ js/questao_03.js
@@ -0,0 +1,22 @@
+// Aluno 1
+let nome1 = "João da Silva";
+let nota1_1 = 7;
+let nota1_2 = 8.5;
+let mult1 = nota1_1 * 0.6;
+let mult2 = nota1_2 * 0.4;
+let soma1 = mult1 + mult2;
+alert(`Aluno: ${nome1}\nNota 1: ${nota1_1}\nNota 2: ${nota1_2}\nMédia: ${soma1}`);
+
+// Aluno 2
+let nome2 = "Maria Parreira";
+let nota2_1 = 6;
+let nota2_2 = 7;
+let soma2 = (nota2_1 * 0.6) + (nota2_2 * 0.4);
+alert(`Aluno: ${nome2}\nNota 1: ${nota2_1}\nNota 2: ${nota2_2}\nMédia: ${soma2}`);
+
+// Aluno 3
+let nome3 = "Carlos Souza";
+let nota3_1 = 8;
+let nota3_2 = 5;
+let soma3 = (nota3_1 * 0.6) + (nota3_2 * 0.4);
+alert(`Aluno: ${nome3}\nNota 1: ${nota3_1}\nNota 2: ${nota3_2}\nMédia: ${soma3}`);
